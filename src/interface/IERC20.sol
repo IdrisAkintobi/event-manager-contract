ad633cc2693944ff6297c77fc9164ad4e8ca6a25
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-09-16 00:53:08 +0100

    moved interface into contracts

--- src/interface/IERC20.sol
+++ src/interface/IERC20.sol
@@ -2,9 +2,5 @@
 pragma solidity ^0.8.26;
 
 interface IERC20 {
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
